6597d49f7338f8f232cafe33298ec3c5f32ec7ff
AkhilBarthwal005 <akhil.barthwal005@gmail.com>
2024-05-31 08:06:36 +0530

    Api Gateway and load balancer implemented

---
+++ src/main/java/com/example/apigatewayandloadbalancer/ApiGatewayAndLoadBalancerApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayandloadbalancer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAndLoadBalancerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayAndLoadBalancerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apigatewayandloadbalancer/ApiGatewayAndLoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayandloadbalancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayAndLoadBalancerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
